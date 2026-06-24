60080cfc307a37aefe2faaa3d8d583c1795a0b27
北邙 <88311117+c1921@users.noreply.github.com>
2026-06-24 11:55:20 +0800

    refactor: 将模式切换按钮从页面头部移至侧边底部
    - 从App.vue中移除ModeToggle组件及其使用
    - 在AppSidebar.vue的SidebarFooter中添加ModeToggle

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -7,10 +7,12 @@ import {
   IconMap,
 } from '@tabler/icons-vue'
 import NavProjects from '@/components/NavProjects.vue'
+import ModeToggle from '@/components/ModeToggle.vue'
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarRail,
 } from '@/components/ui/sidebar'
 
@@ -45,6 +47,11 @@ const data = {
     <SidebarContent>
       <NavProjects :projects="data.projects" />
     </SidebarContent>
+    <SidebarFooter class="border-t p-2">
+      <div class="flex justify-center">
+        <ModeToggle />
+      </div>
+    </SidebarFooter>
     <SidebarRail />
   </Sidebar>
 </template>
